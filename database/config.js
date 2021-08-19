ed80302f460e24c52f70e4da95ac23470691a822
MaTiUs77 <matius77@gmail.com>
2021-08-19 02:07:15 -0300

    Mongo sometimes require USER and PASS
    En caso de requerir autenticacion adicionar al archivo .env
    + MONGO_USER
    + MONGO_PASS

--- database/config.js
+++ database/config.js
@@ -2,12 +2,23 @@ const mongoose = require('mongoose');
 
 const dbConnection = async() => {
     try {
-        await mongoose.connect(process.env.MONGODB_CNN, {
+        let options =  {
             useNewUrlParser: true, 
             useUnifiedTopology: true,
             useCreateIndex: true,
-            useFindAndModify: false
-        });
+            useFindAndModify: false,
+        }
+        
+        if(process.env.MONGO_USER) {
+            console.log(`Mongo user:${process.env.MONGO_USER}`)
+            options = {
+                ...options,
+                auth: {authSource: 'admin'},
+                user: process.env.MONGO_USER,
+                pass: process.env.MONGO_PASS
+            }
+        }
+        await mongoose.connect(process.env.MONGODB_CNN,options);
 
         console.log('Base de datos online');
     } catch (error) {
