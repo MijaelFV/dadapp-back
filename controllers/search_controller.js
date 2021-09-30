dec07ae4b280cac1b88b30fb819d3433c814d624
Mijael Flores Vega <68754287+MijaelFV@users.noreply.github.com>
2021-09-29 23:47:38 -0300

    Added search by category
    #34 Issue resolved

--- controllers/search_controller.js
+++ controllers/search_controller.js
@@ -2,6 +2,7 @@ const Item = require('../models/item_model');
 const Space = require('../models/space_model');
 const User = require('../models/user_model');
 const Area = require('../models/area_model');
+const Category = require('../models/category_model');
 
 const searchGetByQuery = async(req, res) => {
     const {id, type} = req.params;
@@ -20,14 +21,16 @@ const searchGetByQuery = async(req, res) => {
                 JSON.stringify(uid).replace(/[^a-zA-Z0-9]/g, '').substring(3)
             ));
         }
-        
-        const resp = await Item.find({name: {$regex: regex}, takedBy: null})
+
+        const matchedCategories = await Category.distinct("_id", {name: {$regex: regex}, space: uidList})
+
+        const resp = await Item.find().or([{category: matchedCategories}, {name: {$regex: regex}}])
             .where('space')
             .in(uidList)
             .select('-takedBy -takedDate')
             .populate('category', '_id name')
             .populate('space', '_id name') 
-
+            
         return resp
     }
 
